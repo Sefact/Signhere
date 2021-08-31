4a63832cf45d9b5c528a491018e588e651e26d55
User <User@DESKTOP-IOALGFJ>
2021-09-01 08:52:02 +0900

    ssm commit
    TextController / FileUtils

--- src/main/java/com/signhere/main/TextController.java
+++ src/main/java/com/signhere/main/TextController.java
@@ -1,9 +1,21 @@
 package com.signhere.main;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.signhere.beans.ApprovalBean;
+import com.signhere.beans.ApprovalCommentBean;
+import com.signhere.beans.CompanionDeferBean;
+import com.signhere.beans.DocumentBean;
+import com.signhere.beans.ReadingReferenceBean;
+import com.signhere.beans.UserBean;
 import com.signhere.services.FileUtils;
 
 @Controller
@@ -11,4 +23,78 @@ public class TextController {
 	@Autowired
 	private FileUtils fut;
 	private ModelAndView mav;
+	
+	
+	@PostMapping("preview")
+	public List<DocumentBean> preview(@RequestBody List<DocumentBean> dlist) {
+		
+		dlist =fut.mPreview(dlist);
+		
+		return dlist;
+	}
+	
+	@PostMapping("requestdraft")
+	public ModelAndView requestdraft(DocumentBean db) {
+		 mav = fut.mRequestDraft(db);
+		
+		return mav;
+	}
+	
+	@PostMapping("approval")
+	public ModelAndView approval(ApprovalBean ab, ApprovalCommentBean acb) {
+		
+		mav =fut.mApproval(ab, acb);
+				
+		return mav;
+	}
+	
+	@PostMapping("compaonion")
+	public ModelAndView compaonion(DocumentBean db, CompanionDeferBean cdb) {
+		mav = fut.mCompanion(db, cdb);
+		
+		return mav;
+	}
+
+	@PostMapping("defer")
+	public ModelAndView defer(DocumentBean db, CompanionDeferBean cdb) {	
+		mav =fut.mDefer(db, cdb);
+		
+		return mav;
+	}
+
+	@PostMapping("draftCancel")
+	public ModelAndView draftCancel(DocumentBean db) {
+		mav = fut.mDraftCancel(db);
+		
+		return mav;
+	}
+
+	@PostMapping("viewDrafter")
+	public List<UserBean> viewDrafter (@ModelAttribute UserBean ub) {
+		
+		return fut.mViewDraft(ub);
+	}
+
+	@PostMapping("readingAuthority")
+	public ModelAndView readingAuthority(UserBean ub) {
+		
+		mav = fut.mReadingAuthority(ub);
+		
+		return mav;
+	}
+
+	@PostMapping("grantAuthority")
+	public ModelAndView grantAuthority(ReadingReferenceBean rb) {
+		
+		mav = fut.mGrantAuthority(rb); 
+				
+		return mav;
+	}
+	
+//9월1일 수정
+	@PostMapping("documentBox")
+	public List<DocumentBean> documentBox(@ModelAttribute DocumentBean dlist) {
+
+		return fut.mDocumentBox(dlist);
+	}
 }
--- src/main/java/com/signhere/services/FileUtils.java
+++ src/main/java/com/signhere/services/FileUtils.java
@@ -1,11 +1,96 @@
 package com.signhere.services;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.signhere.beans.ApprovalBean;
+import com.signhere.beans.ApprovalCommentBean;
+import com.signhere.beans.CompanionDeferBean;
+import com.signhere.beans.DocumentBean;
+import com.signhere.beans.ReadingReferenceBean;
+import com.signhere.beans.UserBean;
 
 @Service
 public class FileUtils {
 	@Autowired
 	SqlSessionTemplate sqlSession;
+	ModelAndView mav;
+	
+	public List<DocumentBean>  mPreview(List<DocumentBean> dlist) {
+		List<DocumentBean> docList;
+		
+		docList = null;
+		
+		return docList;
+	}
+	
+	
+	public ModelAndView mRequestDraft(DocumentBean db) {
+		mav = new ModelAndView();
+		
+		mav.setViewName("");
+		return mav;
+	}
+	
+	public ModelAndView mApproval(ApprovalBean ab, ApprovalCommentBean acb) {
+		mav = new ModelAndView();
+		
+		mav.setViewName("");
+		return mav;
+	}
+	
+	public ModelAndView mCompanion(DocumentBean db, CompanionDeferBean cdb) {
+		mav = new ModelAndView();
+		
+		mav.setViewName("");
+		return mav;
+	}
+	
+	public ModelAndView mDefer(DocumentBean db, CompanionDeferBean cdb) {
+		mav = new ModelAndView();
+		
+		mav.setViewName("");
+		return mav;
+	}
+	
+	public ModelAndView mDraftCancel(DocumentBean db) {
+		mav = new ModelAndView();
+		
+		mav.setViewName("");
+		return mav;
+	}
+	
+	public List<UserBean> mViewDraft(UserBean ub) {
+		List<UserBean> userList;
+		
+		userList = null;
+		
+		return userList;
+	}
+	
+	public ModelAndView mReadingAuthority(UserBean ub) {
+		mav = new ModelAndView();
+		
+		mav.setViewName("");
+		return mav;
+	}
+	
+	public ModelAndView mGrantAuthority(ReadingReferenceBean rb) {
+		mav = new ModelAndView();
+		
+		mav.setViewName("");
+		return mav;
+	}
+	
+	public List<DocumentBean> mDocumentBox(DocumentBean dlist) {
+		List<DocumentBean> docList;
+		
+		docList = null;
+		return docList;
+	}
+	
 }
