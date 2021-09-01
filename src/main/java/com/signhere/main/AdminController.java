239ca3ce18cc81a2992bdefdbb559dabbde0e704
dytakk <82704585+dytakk@users.noreply.github.com>
2021-09-01 09:47:36 +0900

    config.propertiex file commit (gitignore file for servlet-context.xml modify datasource information)

--- src/main/java/com/signhere/main/AdminController.java
+++ src/main/java/com/signhere/main/AdminController.java
@@ -13,5 +13,47 @@ import com.signhere.services.Management;
 
 
 public class AdminController {
+	@Autowired
+	private Management mag;
+	private ModelAndView mav;
 	
+	//직원 관리 페이지로 이동 
+	@GetMapping("/admin")
+	public String admin() {
+		return "admin";
+	}
+	
+	//직원 관리 페이지에서 새로운 직원 추가
+	@PostMapping("/addEmployee")
+	public ModelAndView addEmployee(@ModelAttribute UserBean ub) {
+		return mag.mAddEmployee(ub);
+	}
+	
+	//새로운 직원 추가시 id 중복 체크 
+	@PostMapping("/employeeDup")
+	public String employeeDup(@RequestParam String userId) {
+		return mag.mEmployeeDup(userId);
+	}
+	
+	//기존 직원 삭제 메소드 
+	@PostMapping("/delEmployee")
+	public ModelAndView delEmployee(@RequestParam String userId) {
+		return mag.mDelEmployee(userId);
+	}
+	
+	@PostMapping("/updateEmployee")
+	public ModelAndView updateEmployee(@ModelAttribute UserBean ub) {
+		return mag.mUpdateEmployee(ub);
+	}
+	
+	@GetMapping("/apListAdmin")
+	public ModelAndView apListAdmin() {
+		return mag.mApListAdmin();
+	}
+	
+	@PostMapping("/apListRemove")
+	public ModelAndView	apListRemove(@RequestParam String dmCode) {
+		//문서 삭제를 한꺼번에 여러개 가능 하게 할경우에 RequestBody(json)나 modelAttribute로 받아야함
+		return mag.mApListRemove(dmCode);
+	}
 }
