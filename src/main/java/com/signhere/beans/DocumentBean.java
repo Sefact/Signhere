2f916ee71862d24cb0cc42b4fa04dbbccd3ebf67
Sefact <lime17442@gmail.com>
2021-09-13 16:41:33 +0900

    Navigation Update

--- src/main/java/com/signhere/beans/DocumentBean.java
+++ src/main/java/com/signhere/beans/DocumentBean.java
@@ -20,6 +20,14 @@ public class DocumentBean {
 	private String dmDate2;
 	private String dmCode;
 	private String dmName;
+	//APPROVALLINE테이블의 APPROVAL_ID가 담길 BEAN
+	private String apId;
+	//REFERENCE 테이블의 REFERENCE_ID가 담길 BEAN
+	private String refId;
+	//REFERENCE 테이블의 READING_ID가 담길 BEAN
+	private String readingId;
+	//APPROVALLINE테이블의 APPROVAL_SEQ가 담길 BEAN
+	private String aplSeq;
 	private char searchCode;
 	private String[] dmNumArr;
 }
